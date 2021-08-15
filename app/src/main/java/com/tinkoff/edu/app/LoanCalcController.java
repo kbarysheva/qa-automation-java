f7e21207ca4ed6c8132b34578a8b0925cb16e8c1
k.barysheva <In483die89Nacht>
2021-08-16 00:27:47 +0300

    homework4 add LoanCalcController LoanCalcService LoanCalcRepository LoanCalcLogger

---
+++ app/src/main/java/com/tinkoff/edu/app/LoanCalcController.java
@@ -0,0 +1,18 @@
+package com.tinkoff.edu.app;
+
+/**
+ *
+ */
+public class LoanCalcController {
+    /**
+     * TODO Validates and logs request
+     */
+    public static int createRequest() {
+        int localVar;
+
+        //param validation
+        //log request
+        LoanCalcLogger.log();
+        return LoanCalcService.createRequest();
+    }
+}
---
+++ app/src/main/java/com/tinkoff/edu/app/LoanCalcService.java
@@ -0,0 +1,11 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcService {
+    /**
+     * TODO Loan calculation
+     */
+    public static int createRequest() {
+        int localVar = 0;
+        return LoanCalcRepository.save();
+    }
+}

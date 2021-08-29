d9fe90d808219c751481a8f3fd644b05b49f79dd
k.barysheva <In483die89Nacht>
2021-08-29 22:35:44 +0300

    Realized 2 tests shouldGetId1WhenFirstCall, shouldGetIncrementedIdWhenAnyCall

--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -1,18 +1,55 @@
 package com.tinkoff.edu;
 
+import com.tinkoff.edu.app.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+    private LoanRequest request;
+    private LoanCalcController sut;
+
+    @BeforeEach
+    public void init(){
+        //region Fixture / Arrange / Given
+        request = new LoanRequest(LoanType.OOO,10, 1_000);
+        sut = new LoanCalcController(new IpNotFriendlyServicePerson(new VariableLoanCalcRepository()));
+        //endregion
+    }
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    @DisplayName("Проверка requestId = 1 после выполнения первого запроса")
+    public void shouldGet1WhenFirstRequest() {
+        //region Fixture / Arrange / Given
+        sut = new LoanCalcController(new IpNotFriendlyServicePerson(new VariableLoanCalcRepository()));
+        //endregion
+
+        //region Act / When
+        LoanResponse loanResponse = sut.createRequest(request); // factual
+        //endregion
+
+        //region Assert / Then
+        assertEquals(1 ,loanResponse.getRequestId());
+        //endregion
+    }
+
+    @Test
+    @DisplayName("Проверка requestId после подмены стартового значения")
+    public void shouldGetIncrementedIdWhenAnyCall() {
+        //region Fixture / Arrange / Given
+        final int startRequestId = 4;
+        sut = new LoanCalcController(new IpNotFriendlyServicePerson(new VariableLoanCalcRepository(startRequestId)));
+        //endregion
+
+        //region Act / When
+        LoanResponse loanResponse = sut.createRequest(request);
+        //endregion
+
+        //region Assert / Then
+        assertEquals(startRequestId + 1 ,loanResponse.getRequestId());
+        //endregion
     }
 }

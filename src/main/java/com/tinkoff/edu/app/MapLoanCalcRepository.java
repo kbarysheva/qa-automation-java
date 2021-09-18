f1f38e465adaf245ecea4f597b6404d3816f24f8
k.barysheva <In483die89Nacht>
2021-09-18 17:15:13 +0300

    add MapLoanCalcRepository

---
+++ src/main/java/com/tinkoff/edu/app/MapLoanCalcRepository.java
@@ -0,0 +1,35 @@
+package com.tinkoff.edu.app;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MapLoanCalcRepository implements LoanCalcRepository {
+
+    private final Map<UUID, LoanResponse> responseMap = new HashMap<>();
+
+    public Map<UUID, LoanResponse> getResponseMap() {
+        return responseMap;
+    }
+
+    public ResponseType getStatusByUUID (Object uuid){
+        if (uuid == null) throw new IllegalArgumentException();
+        return responseMap.get(uuid).getResponseType();
+    }
+
+    public boolean setStatusByUUID(Object uuid, ResponseType responseType){
+        if (uuid == null || responseType == null) throw new IllegalArgumentException();
+        LoanResponse loanResponse = responseMap.get(uuid);
+        loanResponse.setResponseType(responseType);
+        return true;
+    }
+
+    @Override
+    public UUID save(LoanRequest request) {
+        if (request == null) throw new IllegalArgumentException();
+        UUID uuid = UUID.randomUUID();
+        LoanResponse loanResponse = new LoanResponse(uuid,request,ResponseType.APPROVED);
+        responseMap.put(uuid, loanResponse);
+        return uuid;
+    }
+}
